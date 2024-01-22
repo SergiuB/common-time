90212008ee91b6647fa945b371f5a87c6a71bdee
SergiuB <buciuc_sergiu@yahoo.com>
2024-01-22 15:28:20 +0200

    500 error page on the client path

---
+++ app/(client)/error.tsx
@@ -0,0 +1,28 @@
+"use client"; // Error components must be Client Components
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <div className="flex flex-col justify-center place-content-center items-center">
+      <h2 className="text-heading2-bold ">Oops! Something Went Wrong</h2>
+      <p className="pt-4 pb-4">
+        Unfortunately, we've encountered a server error. We're working to
+        resolve the issue as quickly as possible.
+      </p>
+      <p className="pb-4">
+        While we fix this, you might want to head back to{" "}
+        <a
+          className="hover:underline underline-offset-2 text-base-regular-light text-primary-500"
+          href="https://anandamasaj.ro/#contact-us"
+        >
+          anandamasaj.ro
+        </a>{" "}
+        and contact us for an appointment using one of our phone numbers
+        (Whatsapp is best).
+      </p>
+      <Image src="/assets/500.png" alt="logo" width={500} height={500}></Image>
+    </div>
+  );
+}
